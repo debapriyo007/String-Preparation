06b127d196a76d119f5f4a4bbca0d0ed0fc99c18
Debapriyo Das <debapriyo00@gmail.com>
2023-09-27 12:00:17 +0530

    Time: 14 ms (32.42%), Space: 42.9 MB (10.74%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        //create two map.
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        
+       for(Integer i = 0 ;i<s.length();i++){
+           if(map1.put(s.charAt(i),i)!= map2.put(t.charAt(i),i)){
+               return false;
+           }
+       }
+        return true;
+    }
+}
