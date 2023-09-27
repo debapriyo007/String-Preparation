3b71834ad481379769e060700abdb653635f7a4c
Debapriyo Das <debapriyo00@gmail.com>
2023-09-27 12:57:53 +0530

    Time: 7 ms (54.11%), Space: 41.3 MB (54.11%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> stk = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                if(stk.size()>0){
+                    sb.append(ch);
+                }
+                stk.push(ch);
+            }else{
+                stk.pop();
+                if(stk.size()>0){
+                    sb.append(ch);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
